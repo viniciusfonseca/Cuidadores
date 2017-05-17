8ab50dfbaf46535ff4bf2923a929415d4ee31afd
viniciusfonseca <vfonseca1618@gmail.com>
2017-05-17 18:14:09 -0300

    Implementar tela de Perfil

--- js/Pages/Home/Profile.js
+++ js/Pages/Home/Profile.js
@@ -2,41 +2,98 @@ import React from 'react'
 
 import { 
     View, ScrollView, Text,
-    Image
+    Image, Alert
 } from 'react-native'
-import { TabNavigator } from 'react-navigation'
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view'
 
 import { connect } from 'react-redux'
 
-import _s, { TERTIARY_COLOR } from '../../Style'
+import _s, { SECONDARY_COLOR, TERTIARY_COLOR } from '../../Style'
 
 import NavBar from '../../Components/NavBar'
 
 class Profile extends React.Component {
+    static get upperSectionStyle() {
+        return {
+            height: 120, 
+            backgroundColor: TERTIARY_COLOR, 
+            borderBottomWidth: 1, 
+            borderColor: '#DDD',
+            padding: 15
+        }
+    }
+
     constructor(props) {
         super(props)
         this.state = {
-            loading: true
+            loading: true,
+            index: 0,
+            routes: [
+                {
+                    key: '1',
+                    title: 'Dependentes'
+                },
+                {
+                    key: '2',
+                    title: 'Contratos'
+                }
+            ]
         }
     }
+
+    renderScene = ({ route }) => {
+        switch (route.key) {
+            case '1':
+                return <Dependentes />
+            case '2':
+                return <Contratos />
+        }
+    }
+
+    handleChangeTab = index => this.setState({ index })
 
     render() {
         return (
             <View style={_s("flex blank")}>
                 <NavBar enableNavBtn={true} navigation={this.props.navigation} />
                 <View style={_s("flex")}>
-                    <ScrollView>
-                        <View style={{ height: 120, backgroundColor: TERTIARY_COLOR, borderBottomWidth: 1, borderColor: '#DDD' }}>
+                    <View style={_s("flex-row",Profile.upperSectionStyle)}>
+                        <View style={_s("center-a")}>
+                            <Text>TODO: Avatar</Text>
                         </View>
-                    </ScrollView>
+                        <View>
+                            <Text>TODO: Info perfil</Text>
+                        </View>
+                    </View>
+                    <TabViewAnimated
+                        navigationState={this.state}
+                        renderScene={this.renderScene.bind(this)}
+                        renderHeader={props => (
+                            <TabBar 
+                                style={{backgroundColor: SECONDARY_COLOR}} 
+                                labelStyle={{color: '#000'}} 
+                                {...props}/>
+                            )}
+                        onRequestChangeTab={this.handleChangeTab.bind(this)}/>
                 </View>
             </View>
         )
     }
 }
 
+class Dependentes extends React.Component {
+    render() {
+        return <View><Text>TODO: Dependentes</Text></View>
+    }
+}
+
+class Contratos extends React.Component {
+    render() {
+        return <View><Text>TODO: Contratos</Text></View>
+    }
+}
+
 const mapStateToProps = state => ({})
 const ProfilePage = connect(mapStateToProps)(Profile)
-
 
 export default ProfilePage
